8e14c19048c2b4d1a02d32d24fb042174061db23
19SERGIO <sergiolondono55@gmail.com>
2023-10-25 18:28:12 -0500

    interfaz y lectura de nuevo sensor

--- vinculo.py
+++ vinculo.py
@@ -25,8 +25,18 @@ def entrada_principal():
     principal.show()
     app.exec()
 
+#funcion para crear nuevo sensor
+def crear_nuevo():
+    id = principal.line_id.text()
+    nombre = principal.line_nombre.text()
+    min = principal.line_min.text()
+    max = principal.line_max.text()
+    
+
 # boton
 login.btn_ingreso.clicked.connect(entrada_login)
+principal.btn_creae.clicked.connect(crear_nuevo)
+principal.btn_prender.clicked.connect()
 
 # ejecutable
 login.show()
